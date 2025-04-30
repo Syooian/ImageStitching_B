db8f96bb04078a5c6638dd94491790e7ce95aa69
Syooian <msn0126@hotmail.com>
2025-04-30 23:16:52 +0800

    新增測試檔案(Test.py) 安裝matplotlib, imageio 說明文件翻譯

---
+++ Test.py
@@ -0,0 +1,44 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import imageio
+cv2.ocl.setUseOpenCL(False)
+import warnings
+warnings.filterwarnings('ignore')
+
+def main():
+    print("Hello, World!")
+
+    feature_extraction_algo = 'sift'
+
+    feature_to_match = 'bf'
+
+    # Make sure that the train image is the image that will be transformed
+    train_photo = cv2.imread('./'  + 'train.jpg')
+
+    # OpenCV defines the color channel in the order BGR 
+    # Hence converting to RGB for Matplotlib
+    train_photo = cv2.cvtColor(train_photo,cv2.COLOR_BGR2RGB)
+
+    # converting to grayscale
+    train_photo_gray = cv2.cvtColor(train_photo, cv2.COLOR_RGB2GRAY)
+
+    # Do the same for the query image 
+    query_photo = cv2.imread('./'  + 'query.jpg')
+    query_photo = cv2.cvtColor(query_photo,cv2.COLOR_BGR2RGB)
+    query_photo_gray = cv2.cvtColor(query_photo, cv2.COLOR_RGB2GRAY)
+
+    # Now view/plot the images
+    fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, constrained_layout=False, figsize=(16,9))
+    ax1.imshow(query_photo, cmap="gray")
+    ax1.set_xlabel("Query image", fontsize=14)
+
+    ax2.imshow(train_photo, cmap="gray")
+    ax2.set_xlabel("Train image (Image to be transformed)", fontsize=14)
+
+    plt.savefig("./_"+'.jpeg', bbox_inches='tight', dpi=300, optimize=True, format='jpeg')
+
+    plt.show()
+
+if __name__ == "__main__":
+    main()
